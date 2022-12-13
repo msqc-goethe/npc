fc6ae6becee947d5fa7d589a33ee4a72d893e20a
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2022-12-13 12:16:49 +0100

    more pep8 stuff

--- npb_wrapper.py
+++ npb_wrapper.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess
 import codecs
 import sys
+from subprocess import Popen, PIPE
 from mpi4py import MPI
 
 VERBOSE = False
 
 
-def decode_utf8(string):
-    return codecs.decode(string, 'UTF-8')
+def decode_utf8(byte_code):
+    """bytes -> string"""
+    return codecs.decode(byte_code, 'UTF-8')
 
 
 def run_server(cmd, dst, communicator):
-    process = subprocess.Popen(
-        cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    communicator.send('running', dest=dst)
-    stdout, stderr = process.communicate()
-    if stderr:
-        print(f'Server error: {decode_utf8(stderr)}')
-    if VERBOSE:
-        print(f'Server output: {decode_utf8(stdout)}')
+    """Run server command. Notify client side when server is running."""
+    with Popen(cmd.split(), stdout=PIPE, stderr=PIPE) as process:
+        communicator.send('server msg', dest=dst)
+        stdout, stderr = process.communicate()
+        if stderr:
+            print(f'Server error: {decode_utf8(stderr)}')
+        if VERBOSE:
+            print(f'Server output: {decode_utf8(stdout)}')
 
 
 def run_client(cmd, source, communicator):
+    """Run client command."""
     sync = communicator.recv(source=source)
-    process = subprocess.Popen(
-        cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if stderr:
-        print(f'Client error: {decode_utf8(stderr)}')
-    if VERBOSE:
-        print(f'Client received {sync}')
-    print(decode_utf8(stdout))
+    with Popen(cmd.split(), stdout=PIPE, stderr=PIPE) as process:
+        stdout, stderr = process.communicate()
+        if stderr:
+            print(f'Client error: {decode_utf8(stderr)}')
+        if VERBOSE:
+            print(f'Client received {sync}')
+        print(decode_utf8(stdout))
 
 
 if __name__ == "__main__":
@@ -71,7 +72,7 @@ if __name__ == "__main__":
     if my_rank == 0:
         run_server(args.servercmd, target_rank, comm)
     else:
-        clientcmd=args.clientcmd.replace('HOSTNAME', OTHER_NAME)
+        clientcmd = args.clientcmd.replace('HOSTNAME', OTHER_NAME)
         if VERBOSE:
             print(f'clientcmd: {clientcmd}')
         run_client(clientcmd, target_rank, comm)
