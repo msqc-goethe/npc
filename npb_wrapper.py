9e255ea4bef63102cf5a60a82fc90c9e19dc6640
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2023-01-12 09:38:44 +0100

    restructure

--- npb_wrapper.py
+++ npb_wrapper.py
@@ -38,11 +38,11 @@ def decode_utf8(byte_code):
     return codecs.decode(byte_code, 'UTF-8')
 
 
-def run_server(cmd, dst, communicator,kill=False):
+def run_server(cmd, dst, communicator, kill_server=False):
     """Run server command. Notify client side when server is running."""
     with Popen(cmd.split(), stdout=PIPE, stderr=PIPE) as process:
         communicator.send('server msg', dest=dst)
-        if kill:
+        if kill_server:
             sync = communicator.recv(source=dst)
             if VERBOSE:
                 print(f'Server received {sync}')
@@ -52,6 +52,7 @@ def run_server(cmd, dst, communicator,kill=False):
             stdout, stderr = process.communicate()
         if stderr:
             print(f'Server error: {decode_utf8(stderr)}')
+            communicator.Abort()
         if VERBOSE:
             print(f'Server output: {decode_utf8(stdout)}')
 
@@ -70,15 +71,17 @@ def run_client(cmd, source, communicator, stdout_parser=None):
         stdout = decode_utf8(stdout)
         if stdout_parser:
             stdout = stdout_parser.convert_to_format(stdout)
+        stdout = stdout.replace('\n', '')
         print(stdout)
 
 
 def evaluate_repeat_regex(string):
+    """Check input string for repeat keyword and arguments"""
     repeat_regex = '-{1,2}repeat\s(([a-z]|[_,-])+):([0-9])+:([0-9])+:([\-,\+,\*,\/])([0-9])+'
     repeat_string = re.search(repeat_regex, string)
     if repeat_string:
-        s1 = repeat_string.group(0).split()
-        config = s1[1].split(':')
+        tmp_string = repeat_string.group(0).split()
+        config = tmp_string[1].split(':')
         cmd = config[0]
         begin = config[1]
         end = config[2]
@@ -89,8 +92,36 @@ def evaluate_repeat_regex(string):
             print(f'Repeat: {cmd} {begin} {end} {operator} {step}')
         string = re.sub(repeat_regex, 'REPEAT', string, 1)
         return [string, cmd, begin, end, operator, step]
-    else:
-        return
+    return []
+
+
+def run_repeated_client_cmd(cmdstring, dst, communicator, parser_obj=None):
+    """Loop through parametes and perform actual measurement"""
+    run_client.already_synced = False
+    repeat_list = evaluate_repeat_regex(cmdstring)
+    cmdstring = repeat_list[0]
+    cmd = repeat_list[1]
+    begin = int(repeat_list[2])
+    end = int(repeat_list[3])
+    operator = repeat_list[4]
+    step = int(repeat_list[5])
+    i = begin
+    while i <= end:
+        repeat_cmd = '-' + cmd + ' ' + str(i)
+        run_cmd = cmdstring.replace('REPEAT', repeat_cmd)
+
+        if VERBOSE:
+            print(f'client_cmd: {run_cmd}')
+
+        run_client(run_cmd, dst, communicator, parser_obj)
+
+        if operator == '+':
+            i += step
+        elif operator == '*':
+            i *= step
+        else:
+            print('Error computing step size')
+            communicator.Abort()
 
 
 if __name__ == "__main__":
@@ -99,11 +130,11 @@ if __name__ == "__main__":
     parser.add_argument('--servercmd', type=str, help='Server command e.g. --servercmd=[cmd].\
                          Iperf example: iperf -s -D -1.')
     parser.add_argument('--clientcmd', type=str,
-                        help='Client command e.g. --clientcmd=[cmd]. Use HOSTNAME placeholder\
+                        help='Client command e.g. --client_cmd=[cmd]. Use HOSTNAME placeholder\
                         to indicate position of real hostname / address in benchmark string')
     parser.add_argument(
-        '--killserver', action=argparse.BooleanOptionalAction, help='Enable debug output')
-
+        '--killserver', action=argparse.BooleanOptionalAction, help='Terminator server\
+                        side after measurements.')
     parser.add_argument(
         '--verbose', action=argparse.BooleanOptionalAction, help='Enable debug output')
     parser.add_argument('--parser', type=str, help='Use given parser to directly convert\
@@ -113,11 +144,11 @@ if __name__ == "__main__":
     parser.add_argument('--header', nargs='+', type=str,
                         help='Header information')
     args = parser.parse_args()
-
+    KILL = False
     if args.verbose:
         VERBOSE = True
     if args.killserver:
-        kill = True
+        KILL = True
     if args.parser:
         if '.py' in args.parser:
             args.parser = args.parser.replace('.py', '')
@@ -129,6 +160,7 @@ if __name__ == "__main__":
     size = comm.Get_size()
     my_rank = comm.Get_rank()
     my_name = MPI.Get_processor_name()
+
     if size != 2:
         sys.exit()('Benchmark wrapper requires two processes!')
 
@@ -138,42 +170,20 @@ if __name__ == "__main__":
     rreq = comm.irecv(source=target_rank)
     sreq.wait()
     OTHER_NAME = rreq.wait()
+
     if VERBOSE:
         print(f'rank {my_rank}: received hostname {OTHER_NAME}')
 
     if my_rank == 0:
-        run_server(args.servercmd, target_rank, comm,kill=kill)
+        run_server(args.servercmd, target_rank, comm, kill_server=KILL)
     else:
-        clientcmd = args.clientcmd.replace('HOSTNAME', OTHER_NAME)
-        if '-repeat' in clientcmd:
-           run_client.already_synced = False
-           l = evaluate_repeat_regex(clientcmd)
-           clientcmd = l[0]
-           cmd = l[1]
-           begin = int(l[2])
-           end = int(l[3])
-           operator = l[4]
-           step = int(l[5])
-           i = begin
-           while i <= end:
-               repeat_cmd = '-' + cmd + ' ' + str(i)
-               run_cmd = clientcmd.replace('REPEAT', repeat_cmd)
-
-               if VERBOSE:
-                  print(f'clientcmd: {run_cmd}')
-
-               run_client(run_cmd, target_rank, comm,
-                           output_parser if args.parser else None)
-
-               if operator == '+':
-                 i += step
-               elif operator == '*':
-                 i *= step
-               else:
-                    print('Error computing step size')
-                    comm.Abort()
-           if kill:
-                comm.send('kill server msg', dest=0)
+        client_cmd = args.clientcmd.replace('HOSTNAME', OTHER_NAME)
+        if '-repeat' in client_cmd:
+            run_repeated_client_cmd(
+                client_cmd, target_rank, comm, output_parser if args.parser else None)
         else:
-            run_client(clientcmd, target_rank, comm,
-                       output_parser if args.parser else None, kill=kill)
+            run_client.already_synced = False
+            run_client(client_cmd, target_rank, comm,
+                       output_parser if args.parser else None)
+        if KILL:
+            comm.send('kill server msg', dest=0)
