4e3fb2a870acf4fdac4179f41d5302a685097071
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2023-03-07 14:38:55 +0100

    cleanup

--- iperf_parser.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json
-
-
-def to_format(input_string, header=None, out_format=None):
-    """Convert iperf3 output string into one of the available formats i.e. csv or json"""
-    if out_format == 'json':
-        return json.loads(input_string)
